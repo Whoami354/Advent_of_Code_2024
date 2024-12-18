11bbbdac21c9baa9ec984f8a9a889d4819cabf9c
WhoAmI <fabian.gregory98@hotmail.de>
2024-12-18 11:34:16 +0100

    Day 07 finished

--- Day_07/part01.py
+++ Day_07/part01.py
@@ -3,17 +3,17 @@ with open("input.txt", "r") as lines:
     # Zum Beispiel: Daten lesen, schreiben oder verarbeiten
     input = lines.read().split("\n")
 
-def check_possible1(target: int, nums: list[int]) -> bool:
+def check_possible(target: int, nums: list[int]) -> bool:
     if len(nums) == 1:
         return target == nums[0]
 
     num = nums.pop()
 
     if target % num == 0:
-        if check_possible1(target // num, nums[:]):
+        if check_possible(target // num, nums[:]):
             return True
     if target - num >= 0:
-        if check_possible1(target - num, nums[:]):
+        if check_possible(target - num, nums[:]):
             return True
 
     return False
@@ -23,7 +23,7 @@ total_sum = 0
 for line in input:
     target = int(line.split(":")[0])
     nums = list(map(int, line.split(": ")[1].split(" ")))
-    if check_possible1(target, nums[:]):
+    if check_possible(target, nums[:]):
         total_sum += target
 
 print(total_sum)
