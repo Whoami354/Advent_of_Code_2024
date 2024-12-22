1b58571a93520a2d159540109d36a5e75d91a074
WhoAmI <fabian.gregory98@hotmail.de>
2024-12-22 20:48:08 +0100

    Day 11 finished

---
+++ Day_11/part02.py
@@ -0,0 +1,25 @@
+from functools import cache
+
+with open("input.txt", "r") as lines:
+    # Führe hier deine gewünschten Operationen mit der geöffneten Datei durch
+    # Zum Beispiel: Daten lesen, schreiben oder verarbeiten
+    stones = list(map(int, lines.read().strip().split(" ")))
+
+@cache
+def count_stones(val: int, blinks: int) -> int:
+    if blinks == 0:
+        return 1
+    if val == 0:
+        return count_stones(1, blinks - 1)
+
+    str_val = str(val)
+    len_str_val = len(str_val)
+    if len_str_val % 2 == 0:
+        return count_stones(
+            int(str_val[: len_str_val // 2]), blinks - 1
+        ) + count_stones(int(str_val[len_str_val // 2 :]), blinks - 1)
+    return count_stones(val * 2024, blinks - 1)
+
+result = sum(count_stones(s, 75) for s in stones)
+
+print(result)
