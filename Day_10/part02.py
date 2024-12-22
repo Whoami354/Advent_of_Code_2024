5acb246865ae8bccd6ef4fddef47490a7d281d3f
WhoAmI <fabian.gregory98@hotmail.de>
2024-12-22 20:11:16 +0100

    Day 10 finished

---
+++ Day_10/part02.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+with open("input.txt", "r") as lines:
+    # Führe hier deine gewünschten Operationen mit der geöffneten Datei durch
+    # Zum Beispiel: Daten lesen, schreiben oder verarbeiten
+    grid = [list(map(int, line.strip())) for line in lines.readlines()]
+
+rows = len(grid)
+cols = len(grid[0])
+zeros = [(r, c) for r, row in enumerate(grid) for c, val in enumerate(row) if val == 0]
+
+def count_trails(r: int, c: int):
+    queue = deque([(r, c)])
+    count = 0
+
+    while queue:
+        r, c = queue.popleft()
+        if grid[r][c] == 9:
+            count += 1
+            continue
+        for dr, dc in [(-1, 0), (0, -1), (1, 0), (0, 1)]:
+            nr, nc = r + dr, c + dc
+            if 0 <= nr < rows and 0 <= nc < cols and grid[r][c] + 1 == grid[nr][nc]:
+                queue.append((nr, nc))
+    return count
+
+paths = 0
+
+for start in zeros:
+    paths += count_trails(*start)
+
+print(paths)
